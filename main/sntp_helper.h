94f94121ab482e50d441c2b4a3e329d29b9ff487
bill <49013812+drdelgado@users.noreply.github.com>
2019-11-16 19:48:40 -0600

    Moved all sntp functionality to helper functions

---
+++ main/sntp_helper.h
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <time.h>
+#include <sys/time.h>
+#include "esp_sntp.h"
+#include "esp_system.h"
+
+#define RETRY_ATTEMPTS 10
+
+typedef void (*sntp_on_connected_cb_t)(void);
+void _time_sync_notification_cb(struct timeval *tv);
+void sntp_set_on_connected_cb(sntp_on_connected_cb_t cb);
+
+esp_err_t sntp_helper_init(void);
+uint64_t sntp_get_ms_since_epoch(void);
+uint64_t sntp_get_sec_since_epoch(void);
+
+esp_err_t sntp_get_tz_timeinfo(const char* tz, struct tm *timeinfo);
